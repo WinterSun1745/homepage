87ce7552b7d7e842301d4f6dd6e922caef7131fb
WinterSun1745 <gksxodid9528@gmail.com>
2020-08-25 23:42:36 +0900

    js

---
+++ clock.js
@@ -0,0 +1,28 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = document.querySelector("h1");
+
+function getTime(){
+    const date = new Date();
+    const minuts = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours < 10 ? `0${hours}`: hours}:${minuts < 10 ? `0${minuts}`: minuts}:${seconds < 10 ? `0${seconds}`: seconds}`;
+}
+
+function init(){
+    getTime();
+    setInterval(getTime, 1000);
+}
+
+init();
+
+
+
+
+
+
+
+
+
+
+
